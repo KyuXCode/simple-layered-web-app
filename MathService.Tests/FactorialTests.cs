a715c30162c5802b75003271e82b63fcc144821f
Kevin-Yu <kai38547@gmail.com>
2024-01-16 14:59:05 -0500

    Test the factorial logic

---
+++ MathService.Tests/FactorialTests.cs
@@ -0,0 +1,30 @@
+namespace MathService.Tests;
+
+public class FactorialTests
+{
+    [Fact]
+    public void Factorial_BaseValue_0()
+    {
+        Assert.Equal(0, FactorialService.GetFactorialOf(0));
+    }
+
+    [Fact]
+    public void Factorial_BaseValue_1()
+    {
+        Assert.Equal(1, FactorialService.GetFactorialOf(1));
+    }
+
+    [Fact]
+    public void Factorial_Of_Positive_Num()
+    {
+        Assert.Equal(2, FactorialService.GetFactorialOf(2));
+    }
+
+    [Fact]
+    public void Factorial_Of_Negative_Num()
+    {
+        Assert.Throws<ArgumentException>(() => FactorialService.GetFactorialOf(-1));
+    }
+
+
+}
--- MathService/FactorialService.cs
+++ MathService/FactorialService.cs
@@ -1,11 +1,20 @@
-﻿namespace MathService;
-
-public class FactorialService
+﻿namespace MathService
 {
-    public static int GetFactorialOf(int n) {
-        if(n==1||n==0) {
-            return 1;
+    public class FactorialService
+    {
+        public static int GetFactorialOf(int n) 
+        {
+            if( n < 0)
+            {
+                throw new ArgumentException("Can't get factorial of a negative integer.");
+
+            }
+
+            if(n == 0 || n == 1) 
+            {
+                return 1;
+            }
+            return n * GetFactorialOf(n - 1);
         }
-        return GetFactorialOf(n) * GetFactorialOf(n-1)
-        }
+    }
 }
