d7fc480bd717666e733aa4ff53d1ff5eb9b3f31e
Kevin-Yu <kai38547@gmail.com>
2024-01-16 13:40:41 -0500

    Initial commit with factorial logic

---
+++ MathService/FactorialService.cs
@@ -0,0 +1,11 @@
+﻿namespace MathService;
+
+public class FactorialService
+{
+    public static int GetFactorialOf(int n) {
+        if(n==1||n==0) {
+            return 1;
+        }
+        return GetFactorialOf(n) * GetFactorialOf(n-1)
+        }
+}
